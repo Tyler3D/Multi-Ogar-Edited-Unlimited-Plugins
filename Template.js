68db1afc269ff344cbf9e5c5883bef638ced571c
Tyler3D <Tylermanrique@gmail.com>
2017-03-18 23:41:29 -0400

    Update Template.js

--- Template.js
+++ Template.js
@@ -1,4 +1,4 @@
-
+// Example plugin Make sure if you are using multiple files, they are in a seprate folder named the plugin's name.
 function Example(gameServer, PluginHandler, Logger) {
   this.gameServer = gameServer;
   this.Logger = Logger;
@@ -13,9 +13,13 @@ module.exports = Example;
 Example.prototype.start = function () {
   // Replace Functions. This is called by PluginHandler
   this.Logger("HI", this.name); // Says Hi
-  this.PluginHandler.addCommand(name, function(gameServer, split) {
-    var id = split[1];
+  this.PluginHandler.addCommand("example", function(gameServer, split) {
+    var id = split[1]; // Id part not neccasary
     // Command Stuff
   
+  });
+  this.PluginHandler.addPlayerCommand("example", function(PlayerCommand, args, playerTracker, gameServer) {
+      var id = args[1]; // Id part not neccasary 
+      // Command Stuff
   });
 };
